3b55bf15e0737d8cc89564ac0ceab66aed760faa
tildozer <actninswitch@gmail.com>
2025-04-27 16:51:06 -0600

    added scrolling functionality to change weapons

--- scripts/weapon/weapon_handler.gd
+++ scripts/weapon/weapon_handler.gd
@@ -3,6 +3,8 @@ extends Node3D
 @export var weapon_1: Node3D
 @export var weapon_2: Node3D
 
+# @export var weapons: Array[Node3D]
+
 func _ready() -> void:
 	equip(weapon_1)
 
@@ -18,5 +20,25 @@ func equip(active_weapon: Node3D) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("weapon_1"):
 		equip(weapon_1)
-	elif event.is_action_pressed("weapon_2"):
+	if event.is_action_pressed("weapon_2"):
 		equip(weapon_2)
+	if event.is_action_pressed("next_weapon"):
+		next_weapon(true)
+	if event.is_action_pressed("prev_weapon"):
+		next_weapon(false)
+
+func next_weapon(next: bool) -> void:
+	var idx = get_current_idx()
+	if next:
+		idx = wrap(idx + 1, 0, get_child_count())
+		equip(get_child(idx))
+	else: 
+		idx = wrap(idx - 1, 0, get_child_count())
+		equip(get_child(idx))
+
+func get_current_idx() -> int:
+	var total_weapons = get_child_count()
+	for idx in total_weapons:
+		if get_child(idx).visible:
+			return idx
+	return 0
